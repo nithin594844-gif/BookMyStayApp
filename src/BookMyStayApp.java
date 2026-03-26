76dffd296da1476aa4fc883e77f61bf870d86462
nithin594844-gif <nithin.5948.44@gmail.com>
2026-03-26 11:21:52 +0530

    UC4: Room Search & Availability Check

--- src/BookMyStayApp.java
+++ src/BookMyStayApp.java
@@ -1,33 +1,34 @@
 import java.util.HashMap;
 import java.util.Map;
 
-class RoomInventory{
+class RoomInventory {
     private Map<String, Integer> roomAvailability;
-    public RoomInventory(){
+    public RoomInventory() {
         roomAvailability = new HashMap<>();
-        initializeInventory();
-    }
-    private void initializeInventory(){
         roomAvailability.put("Single Room", 5);
         roomAvailability.put("Double Room", 3);
         roomAvailability.put("Suite Room", 2);
     }
-    public Map<String, Integer> getRoomAvailability(){
+    public Map<String, Integer> getRoomAvailability() {
         return roomAvailability;
     }
-    public void updateAvailability(String roomType, int count){
-        roomAvailability.put(roomType, count);
+}
+class RoomSearchService {
+    public void searchAvailableRooms(RoomInventory inventory) {
+        Map<String, Integer> availability = inventory.getRoomAvailability();
+        if (availability.get("Single Room") > 0)
+            System.out.println("Single rooms are available");
+        if (availability.get("Double Room") > 0)
+            System.out.println("Double rooms are available");
+        if (availability.get("Suite Room") > 0)
+            System.out.println("Suite rooms are available");
     }
 }
 public class BookMyStayApp {
-    public static void main(String[] args){
-        System.out.println("Hotel Room Inventory Status");
-        RoomInventory ri = new RoomInventory();
-        Map<String, Integer> rooms = ri.getRoomAvailability();
-        for(String roomType : rooms.keySet()) {
-            System.out.println("\n" + roomType + ";");
-            System.out.println("Available: " + rooms.get(roomType));
-        }
+    public static void main(String[] args) {
+        System.out.println("Room Search");
+        RoomInventory inventory = new RoomInventory();
+        RoomSearchService rss = new RoomSearchService();
+        rss.searchAvailableRooms(inventory);
     }
 }
-
